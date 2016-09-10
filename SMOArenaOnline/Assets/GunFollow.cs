47b8c4288b85dac6e090f66b6f5137080cc3ea32
spuller7 <spuller7@gmail.com>
2016-09-10 01:40:19 -0400

    First Commit
    Adding initial unity project with basic map and moving mechanics

---
+++ SMOArenaOnline/Assets/GunFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunFollow : MonoBehaviour {
+
+    //Public Vars
+    public Camera camera;
+    public float speed;
+    public Rigidbody rigidbody;
+
+    //Private Vars
+    private Vector3 mousePosition;
+    private Vector3 direction;
+    private float distanceFromObject;
+
+    void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+
+      
+
+        }
+
+    }
+
+     
+ 
---
+++ SMOArenaOnline/Assets/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private Rotate rotate;
+    [SerializeField]
+    private float speed = .5f;
+    [SerializeField]
+    private float thrusterForce = 1000f;
+    [Header("Spring Settings:")]
+    [SerializeField]
+    private float jointSpring;
+    [SerializeField]
+    private float jointMaxForce;
+    
+
+    private ConfigurableJoint joint;
+    private PlayerMotor motor;
+    
+
+	void Start () {
+        motor = GetComponent<PlayerMotor>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        SetJointSettings(jointSpring);
+	}
+
+	void Update () {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        rotate.setXMov(_xMov);
+        Vector3 _movHorizontal = Vector3.right * _xMov * speed;
+        //Apply left/right movement
+       motor.move(_movHorizontal);
+       
+        //calculate thruster force based on input
+        Vector3 _thrusterForce = Vector3.zero;
+
+        //Apply thruster force
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+            SetJointSettings(0f);
+        }else
+        {
+            SetJointSettings(jointSpring);
+        }
+        motor.ApplyThruster(_thrusterForce);
+	}
+    private void SetJointSettings(float _jointSpring)
+    {
+        joint.yDrive = new JointDrive {
+            positionSpring = jointSpring,
+            maximumForce = jointMaxForce
+        };
+
+    }
+
+    private void flipCharacter()
+    {
+
+    }
+
+}
