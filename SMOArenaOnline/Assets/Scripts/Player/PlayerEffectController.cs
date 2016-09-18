788c60b36a6c8fce4cd6987c993fdb58159c55af
Spuller <spuller7@gmail.com>
2016-09-18 01:17:09 -0400

    Included boost to model

---
+++ SMOArenaOnline/Assets/Scripts/Player/PlayerEffectController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEffectController : MonoBehaviour {
+
+    [SerializeField]
+    ParticleSystem[] effectsToDisable;
+    [SerializeField]
+    private bool isEnabled = false;
+    [SerializeField]
+    Light[] lightToDisable;
+
+    void Start()
+    {
+        disableEffect();
+    }
+
+    public void disableEffect(){
+        isEnabled = false;
+        for (int i = 0; i < effectsToDisable.Length; i++)
+        {
+            if (effectsToDisable[i].isPlaying)
+            {
+                effectsToDisable[i].Pause();
+
+                effectsToDisable[i].enableEmission = false;
+                Debug.Log("Disabled");
+            }
+            else
+            {
+                Debug.Log("Not playing");
+            }
+        }
+        for (int i = 0; i < lightToDisable.Length; i++)
+        {
+            lightToDisable[i].enabled = false;
+        }
+
+    }
+    /*
+    public void enableEffect()
+    {
+        isEnabled = true;
+        for (int i = 0; i < effectsToDisable.Length; i++)
+        {
+            Debug.Log("Enabled");
+            effectsToDisable[i].enableEmission = true;
+        }
+        for (int i = 0; i < lightToDisable.Length; i++)
+        {
+            lightToDisable[i].enabled = true;
+        }
+    }*/
+    public bool getEffectActivity()
+    {
+        return isEnabled;
+    }
+}
---
+++ SMOArenaOnline/Assets/Scripts/Player/PlayerEffects.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEffects : MonoBehaviour {
+
+    public bool isBoosting;
+    public PlayerEffectController boostEffect;
+    [SerializeField]
+    private GameObject boostEffectGameobject;
+    [SerializeField]
+    private GameObject gunFlareEffectGameobject;
+
+    void Start()
+    {
+        boostEffect = boostEffectGameobject.GetComponent<PlayerEffectController>();
+        isBoosting = false;
+    }
+
+
+    void Update()
+    {
+        handleBoost();
+    }
+
+    private void handleBoost()
+    {
+        if(isBoosting == true)
+        {
+            if(boostEffect.getEffectActivity() == false)
+            {
+              //  boostEffect.enableEffect();
+            }
+        }
+        else
+        {
+            if(boostEffect.getEffectActivity() == true)
+            {
+                boostEffect.disableEffect();
+            }
+        }
+    }
+}
